9b9bcc520ea23cb8f12455a53eb9abdd6f94b862
Behnam Azimi <eng.behnamazimi@gmail.com>
2020-03-30 10:40:17 +0300

    comments added

--- web-chat-app/scripts/data-factory.js
+++ web-chat-app/scripts/data-factory.js
@@ -1,5 +1,4 @@
-const names = ["Mario Speedwagon", "Petey Cruiser", "Anna Sthesia", "Paul Molive", "Anna Mull", "Gail Forcewind", "Paige Turner", "Bob Frapples", "Walter Melon", "Nick R. Bocker", "Barb Ackue", "Buck Kinnear", "Greta Life", "Ira Membrit", "Shonda Leer", "Brock Lee", "Maya Didas", "Rick O'Shea", "Pete Sariya", "Monty Carlo", "Sal Monella", "Sue Vaneer", "Cliff Hanger", "Barb Dwyer", "Terry Aki", "Cory Ander", "Robin Banks", "Jimmy Changa", "Barry Wine", "Wilma Mumduya", "Buster Hyman", "Poppa Cherry", "Zack Lee", "Don Stairs", "Saul T. Balls", "Peter Pants", "Hal Appeno", "Otto Matic", "Moe Fugga", "Graham Cracker", "Tom Foolery", "Al Dente", "Bud Wiser", "Polly Tech", "Holly Graham", "Frank N. Stein", "Cam L. Toe", "Pat Agonia", "Tara Zona", "Barry Cade"]
-
+// generate random date
 function randomDate() {
     const start = 1585008324467;
     const end = new Date().getTime();
@@ -10,11 +9,14 @@ function randomDate() {
     return date;
 }
 
+// generate random number in range
 function randomNumber(max, min = 0) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+// generate chat object
 function chatGenerator(index) {
+    const names = ["Mario Speedwagon", "Petey Cruiser", "Anna Sthesia", "Paul Molive", "Anna Mull", "Gail Forcewind", "Paige Turner", "Bob Frapples", "Walter Melon", "Nick R. Bocker", "Barb Ackue", "Buck Kinnear", "Greta Life", "Ira Membrit", "Shonda Leer", "Brock Lee", "Maya Didas", "Rick O'Shea", "Pete Sariya", "Monty Carlo", "Sal Monella", "Sue Vaneer", "Cliff Hanger", "Barb Dwyer", "Terry Aki", "Cory Ander", "Robin Banks", "Jimmy Changa", "Barry Wine", "Wilma Mumduya", "Buster Hyman", "Poppa Cherry", "Zack Lee", "Don Stairs", "Saul T. Balls", "Peter Pants", "Hal Appeno", "Otto Matic", "Moe Fugga", "Graham Cracker", "Tom Foolery", "Al Dente", "Bud Wiser", "Polly Tech", "Holly Graham", "Frank N. Stein", "Cam L. Toe", "Pat Agonia", "Tara Zona", "Barry Cade"]
     const name = names[index];
     let lastseen = randomDate().toLocaleDateString().replace(/\//g, ".");
     if (index % 3 === 0)
@@ -33,8 +35,10 @@ function chatGenerator(index) {
     }
 }
 
-function getRandomText(meanLess = false) {
-    if (meanLess) {
+// generate random sentences for messages
+function getRandomText(sub = false) {
+    // sub-string a long paragraph.
+    if (sub) {
         const lorem = `If the family member doesn’t need hospitalization and can be cared for at home, you should help him or her with basic needs and monitor the symptoms, while also keeping as much distance as possible, according to guidelines issued by the C.D.C. If there’s space, the sick family member should stay in a separate room and use a separate bathroom. If masks are available, both the sick person and the caregiver should wear them when the caregiver enters the room. Make sure not to share any dishes or other household items and to regularly clean surfaces like counters, doorknobs, toilets and tables. Don’t forget to wash your hands frequently.`;
         const i1 = randomNumber(lorem.length, 6);
         const i2 = randomNumber(lorem.length, 6);
@@ -42,6 +46,8 @@ function getRandomText(meanLess = false) {
         const end = Math.min(i2, i1);
         return lorem.substr(start, end)
     }
+
+    // make a sentences of random words
     let verbs, nouns, adjectives, adverbs, preposition;
     nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog",
         "area", "book", "business", "case", "child", "company", "country", "day", "eye",
