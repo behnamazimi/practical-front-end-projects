fde51d330ca123d33c034fd434e3e846bb1d2b7b
Behnam Azimi <eng.behnamazimi@gmail.com>
2020-04-12 20:28:27 +0300

    add audio message control

--- web-chat-app/components/chat-message.js
+++ web-chat-app/components/chat-message.js
@@ -77,7 +77,7 @@ class ChatMessage extends Component {
                     margin-right: .5rem;
                     margin-left: auto;
                     width: auto;
-                    min-width: 150px;
+                    min-width: 180px;
                     background-color: var(--primaryColor);
                     color: #fff;
                     box-shadow: -1px 1px 3px 0 rgba(0, 0, 0, 0.14);
@@ -123,7 +123,14 @@ class ChatMessage extends Component {
                     right: .5em;
                     bottom: .3em;
                 }
-                #audio-play {
+                .audio-control {
+                    display: none;
+                    align-items: center;
+                }
+                .audio-control button{
+                    margin-right: .5rem;
+                }
+                .audio-control #audio-play {
                     background-color: transparent;
                     border-radius: 50%;
                     border: 1px solid #fff;
@@ -136,20 +143,23 @@ class ChatMessage extends Component {
                     outline: none;
                     cursor: pointer;
                 }
-                #audio-play svg {
+                .audio-control #audio-play svg {
                     margin-left: 2px;
                 }
-                #audio-play .pause-icon{
+                .audio-control #audio-play .pause-icon {
                     display: none;
                 }
-                #audio-play.playing .play-icon{
+                .audio-control #audio-play.playing .play-icon{
                     display: none;
                 }
-                #audio-play.playing .pause-icon{
+                .audio-control #audio-play.playing .pause-icon{
                     display: inline-block;
                     margin-left: 0;
                 }
-                
+                .audio-control #audio-duration {
+                    font-size: 14px;
+                    opacity: .8;
+                }
                
                 </style>`)
     }
@@ -163,19 +173,22 @@ class ChatMessage extends Component {
             <template>
                 ${ChatMessage.style}
                 <p id="text"></p>
-                <button id="audio-play">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                         class="play-icon">
-                        <polygon points="5 3 19 12 5 21 5 3"></polygon>
-                    </svg>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                         class="pause-icon">
-                            <rect x="6" y="4" width="4" height="16"></rect>
-                            <rect x="14" y="4" width="4" height="16"></rect>
+                <div class="audio-control">
+                    <button id="audio-play">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="play-icon">
+                            <polygon points="5 3 19 12 5 21 5 3"></polygon>
                         </svg>
-                </button>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="pause-icon">
+                                <rect x="6" y="4" width="4" height="16"></rect>
+                                <rect x="14" y="4" width="4" height="16"></rect>
+                            </svg>
+                    </button>
+                    <span id="audio-duration">00:00</span>
+                </div>
                 <span id="time"></span>
             </template>
             `)
@@ -189,7 +202,10 @@ class ChatMessage extends Component {
 
         this._textElement = this.shadowRoot.getElementById("text");
         this._timeElement = this.shadowRoot.getElementById("time");
+
+        this._audioMessageCtrl = this.shadowRoot.querySelector(".audio-control");
         this._audioPlayBtn = this.shadowRoot.getElementById("audio-play");
+        this._audioDurationElm = this.shadowRoot.getElementById("audio-duration");
 
     }
 
@@ -228,30 +244,52 @@ class ChatMessage extends Component {
 
     /**
      * reflect the audio attr on HTML tag
-     * @param value
+     * @param audioObj
      */
-    set audio(value) {
-        if (value) {
-            this.setAttribute('audio', value);
-            this._audioElement = new Audio(value);
+    set audio(audioObj) {
+        if (audioObj) {
+
+            // set received object as _audio and do some process on it and
+            // add some listeners to control the play status or timing
+            this._audio = audioObj;
+            const {audio, duration, audioUrl} = audioObj;
+            this._audioElement = audio || new Audio(audioUrl);
+
+            this._audioDurationElm.innerText = duration;
 
             const onPause = () => {
+                // handle icon changing for btn
                 this._audioPlayBtn.classList.remove("playing");
+            };
+
+            const onEnded = () => {
+                // reset the duration text and handle icon changing for btn
+                this._audioDurationElm.innerText = duration;
+                this._audioPlayBtn.classList.remove("playing");
+            };
+
+            const onTimeUpdate = () => {
+                // calc duration and update the text when audio is playing
+                const passedTime = Recorder.secToTimeStr(this._audioElement.currentTime);
+                this._audioDurationElm.innerText = `${passedTime} / ${duration}`;
             };
 
             const onPlay = () => {
                 this._audioPlayBtn.classList.add("playing");
             };
-            this._audioElement.onended = onPause;
+            this._audioElement.onended = onEnded;
             this._audioElement.onpause = onPause;
             this._audioElement.onplay = onPlay;
+            this._audioElement.ontimeupdate = onTimeUpdate;
+
         } else {
-            this.removeAttribute('audio');
+            // set the _audio as null to remove it from component
+            this._audio = null;
         }
     }
 
     get audio() {
-        return this.getAttribute('audio');
+        return this._audio;
     }
 
     setTimeObject(value) {
@@ -310,6 +348,10 @@ class ChatMessage extends Component {
         return this.hasAttribute('lastingroup');
     }
 
+    /**
+     * fire when audio message play btn clicked and toggle the playing status
+     * @private
+     */
     _onAudioPlayBtnClick() {
         if (!this._audioElement)
             return;
@@ -327,7 +369,7 @@ class ChatMessage extends Component {
     render() {
         this._textElement.innerHTML = this.text;
         this._timeElement.innerHTML = this.time;
-        this._audioPlayBtn.style.display = this.audio ? "flex" : "none";
+        this._audioMessageCtrl.style.display = this.audio ? "flex" : "none";
     }
 
 }
