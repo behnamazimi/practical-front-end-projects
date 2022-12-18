811645eeb80ac70758e2964f26197988fc8eed42
avanishEpam <118430406+avanishEpam@users.noreply.github.com>
2022-12-18 14:25:02 +0530

    Move Event To Function
    Create addMouseEvent Method to use in future with basic requirement

--- 09.split-screen/scripts/main.js
+++ 09.split-screen/scripts/main.js
@@ -2,18 +2,15 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
 
-left.addEventListener("mouseenter", () => {
-  container.classList.add("hover-left");
-});
+function addEvent(ele, className){
+	ele.addEventListener("mouseenter", () => {
+	  container.classList.add(className);
+	});
+	
+	ele.addEventListener("mouseleave", () => {
+	  container.classList.remove(className);
+	});
+}
 
-left.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-left");
-});
-
-right.addEventListener("mouseenter", () => {
-  container.classList.add("hover-right");
-});
-
-right.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-right");
-});
+addEvent(left, "hover-left");
+addEvent(right, "hover-right");
